03bcfc1a7c34d93c598f11c9b8890d004da514bf
limstash <limstash.w@gmail.com>
2019-12-21 15:15:02 +0800

    rename uoj.js to qoj.js

--- app/views/page-header.php
+++ app/views/page-header.php
@@ -78,7 +78,7 @@
 		<?= HTML::js_src('/js/color-converter.min.js') ?>
 		
 		<!-- uoj -->
-		<?= HTML::js_src('/js/uoj.js?v=2019.12.20') ?>
+		<?= HTML::js_src('/js/qoj.js?v=2019.12.21') ?>
 		
 		<!-- readmore -->
 		<?= HTML::js_src('/js/readmore/readmore.min.js') ?>
