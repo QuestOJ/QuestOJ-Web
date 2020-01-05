cceba3584bc2555c696f2203201379245fe69e2f
Limstash Wong <limstash.w@gmail.com>
2020-01-05 15:42:16 +0800

    update api verify

--- app/controllers/api/verify.php
+++ app/controllers/api/verify.php
@@ -8,12 +8,17 @@
         die("Please provide server secret");
     }
 
+    if (empty($_POST["request"])) {
+        die("Please provide request id");
+    }
+
     if (empty($_POST["action"])) {
         die("Please provide request action");
     }
 
     $token = DB::escape($_POST["token"]);
     $secret = DB::escape($_POST["secret"]);
+    $request = DB::escape($_POST["request"]);
     $action = DB::escape($_POST["action"]);
 
     if (!API::checkClient($token, $secret)) {
