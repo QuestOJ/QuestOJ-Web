bef67dda72518ac9c78357308d932c30f8590882
limstash <limstash.w@gmail.com>
2020-07-30 18:49:04 +0800

    (chore) create a new tab for blogs and docs

--- app/views/main-nav.php
+++ app/views/main-nav.php
@@ -11,8 +11,8 @@
 			<?php if (!UOJConfig::$data['switch']['disable-hack']): ?>
 			<li class="nav-item"><a class="nav-link" href="<?= HTML::url('/hacks') ?>"><span class="glyphicon glyphicon-flag"></span> <?= UOJLocale::get('hacks') ?></a></li>
 			<?php endif ?>
-			<li class="nav-item"><a class="nav-link" href="<?= UOJConfig::$data['wordpress']['address'] ?>"><span class="glyphicon glyphicon-edit"></span> <?= UOJLocale::get('blogs') ?></a></li>
-			<li class="nav-item"><a class="nav-link" href="<?= UOJConfig::$data['profile']['docs-url'] ?>"><span class="glyphicon glyphicon-info-sign"></span> <?= UOJLocale::get('help') ?></a></li>
+			<li class="nav-item"><a class="nav-link" href="<?= UOJConfig::$data['wordpress']['address'] ?>" target="_blank"><span class="glyphicon glyphicon-edit"></span> <?= UOJLocale::get('blogs') ?></a></li>
+			<li class="nav-item"><a class="nav-link" href="<?= UOJConfig::$data['profile']['docs-url'] ?>" target="_blank"><span class="glyphicon glyphicon-info-sign"></span> <?= UOJLocale::get('help') ?></a></li>
 		</ul>
 		<form id="form-search-problem" class="form-inline my-2 my-lg-0" method="get">
 			 <div class="input-group">
